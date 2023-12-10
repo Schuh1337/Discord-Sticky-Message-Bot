f02e0644b4649f1b7d59d944484b2af973056ddb
Schuh <141640357+Schuh1337@users.noreply.github.com>
2023-12-10 21:31:56 +0100

    Update sticky.py

--- sticky.py
+++ sticky.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix='IRRELEVANT', intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
